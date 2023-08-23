8e56f22a4acb609effc5aff01a053633541c0185
nishiharu <checompetition@163.com>
2023-08-23 07:19:56 +0000

    demo DONE

--- kankan/backend.py
+++ kankan/backend.py
@@ -1,6 +1,17 @@
+import sys
+sys.path.append('.')
+
 from fastapi import FastAPI
 from pydantic import BaseModel
+from array import array
 import uvicorn
+import os
+import heapq
+
+import openai
+from openai.embeddings_utils import get_embedding
+
+from vecscan.vecscan import PyVectorScanner
 
 class KankanRequest(BaseModel):
     tdfk: str
@@ -9,20 +20,89 @@ class KankanRequest(BaseModel):
 class KankanAPI(FastAPI):
     def __init__(self):
         super().__init__()
+        openai.api_key=os.environ["OPENAI_API_KEY"]
 
+        self.scanners = {}
+        self.raw_data = {}
+        self.vec_file_path_base = '/home/01052711/kankan/dataset/sentvecs.ada.kuchikomi_report.vec'
+        self.raw_file_path_base = '/home/01052711/kankan/dataset/jalan_kanko.csv'
+        self.n_best_hotel = 10
+        self.n_best_kuchikomi = 10
         self.post("/api/search")(self.search)
 
     def search(self, req: KankanRequest):
-        res = {
-            'result': req.tdfk + '|||' + req.query
-        }
-        return res
+        if req.tdfk not in self.scanners:
+            self.scanners[req.tdfk] = PyVectorScanner(f'{self.vec_file_path_base}.{req.tdfk}', '')
+            self.raw_data[req.tdfk] = []
+            for line_idx, line in enumerate(open(f'{self.raw_file_path_base}.{req.tdfk}.csv')):
+                if line_idx > 0:
+                    self.raw_data[req.tdfk].append(line.strip())
+        
+        scan_results = self.scanners[req.tdfk].n_best_vecs(
+            query_vec=array('f', get_embedding(req.query, engine='text-embedding-ada-002')),
+            n_best=100000,
+            openmp=True,
+            debug=True,
+            threshold=0.50
+        )[0]
+
+        spot_result = {}
+        full_odk_set = set()
+        scan_results_len = scan_results.size()
+        for i in range(scan_results_len):
+            idx, score = scan_results.get(i)
+            fields = self.raw_data[req.tdfk][idx].strip().split(',')
+            kuchikomi_id, odk_id, other = fields[0], fields[1], ','.join(fields[2:])
+            if odk_id not in spot_result:
+                spot_result[odk_id] = {'score': 0.0, 'kuchikomi': [], 'cnt': 0}
+            if spot_result[odk_id]['cnt'] < self.n_best_kuchikomi:
+                spot_result[odk_id]['kuchikomi'].append((score, other))
+                spot_result[odk_id]['score'] += score
+                if spot_result[odk_id]['cnt'] > 0:
+                    spot_result[odk_id]['score'] /= 2
+                spot_result[odk_id]['cnt'] += 1
+            if spot_result[odk_id]['cnt'] == self.n_best_kuchikomi:
+                full_odk_set.add(odk_id)
+                if len(full_odk_set) >= self.n_best_hotel:
+                    break
+
+        if len(full_odk_set) == self.n_best_hotel:
+            result_list = [spot_result[k] for k in full_odk_set]
+        else:
+            result_list = spot_result.values()
+        result_list = heapq.nlargest(self.n_best_hotel, result_list, key=lambda x: x['score'])
+
+        ret = {'hotel': []}
+        for spot_rank, kuchikomi_list in enumerate(result_list):
+            ret['hotel'].append(
+                {
+                    'rank': spot_rank + 1,
+                    'score': kuchikomi_list['score'],
+                    'kuchikomi': []
+                }
+            )
+            for kuchikomi in kuchikomi_list['kuchikomi']:
+                fields = kuchikomi[1].split(',')
+                ret['hotel'][-1]['kuchikomi'].append(
+                    {
+                        'score': kuchikomi[0],
+                        'rate': fields[0],
+                        'title': fields[1],
+                        'text': ','.join(fields[2:-5]),
+                        'date': fields[-5] + '/' + fields[-4],
+                        'name': fields[-3],
+                        'address': fields[-2],
+                        'ybn': fields[-1]
+                    }
+                )
+
+        return ret
     
 def main():
     app = KankanAPI()
     uvicorn.run(
         app,
-        port=21345,
+        port=21344,
         root_path='/app/kankan'
     )
 
--- kankan/frontend.py
+++ kankan/frontend.py
@@ -1,17 +1,43 @@
 import streamlit as st
 import requests
+import json
+
+def kanji2romaji(s):
+    if s == '東京都':
+        return 'tokyo'
+    elif s == '大阪府':
+        return 'osaka'
+    elif s == '長野県':
+        return 'nagano'
+    elif s == '沖縄県':
+        return 'okinawa'
+    else:
+        return 'japan'
+
+def json2str(res_dict):
+    res_lines = []
+    for hotel_info in res_dict['hotel']:
+        res_lines.append('*'*40)
+        res_lines.append(
+            '第{}位：{}。類似度スコア：{}。住所：〒{} {}'.format(
+                hotel_info['rank'], hotel_info['kuchikomi'][0]['name'], hotel_info['score'], hotel_info['kuchikomi'][0]['ybn'], hotel_info['kuchikomi'][0]['address']
+            )
+        )
+        res_lines.append('*'*40)
+        for kuchikomi in hotel_info['kuchikomi']:
+            res_lines.append('【{:.4f}】{}'.format(kuchikomi['score'], kuchikomi['text']))
+    return '\n'.join(res_lines)
 
 def search_hotel():
     response = requests.post(
-        'http://localhost:21345/api/search',
+        'http://localhost:21344/api/search',
         json={
-            'tdfk': st.session_state['tdfk'],
+            'tdfk': kanji2romaji(st.session_state['tdfk']),
             'query': st.session_state['query']
         }
     )
     if response.status_code == 200:
-        response = response.json()
-        st.session_state['result'] = '***  {}   ***'.format(response['result'])
+        st.session_state['result'] = json2str(response.json())
 
 def update_state(key):
     st.session_state[key] = st.session_state[key]
@@ -23,10 +49,20 @@ def main():
     )
     st.title('KanKan：観光スポット検索エンジン')
 
-    st.selectbox('都道府県',('日本全国', '東京都', '大阪府', '長野県', '沖縄県'), key='tdfk', on_change=update_state, args=('tdfk',))
-    st.text_input('検索クエリ', 'デートに最適', key='query', on_change=update_state, args=('query',))
-    st.button('検索', on_click=search_hotel)
-    st.text_area('検索結果', '', key='result', on_change=update_state, args=('result',))
+    col1, col2 = st.columns([1,5])
+
+    with col1:
+        st.selectbox(
+            '都道府県',
+            ('東京都', '大阪府', '長野県', '沖縄県'), 
+            key='tdfk', on_change=update_state, args=('tdfk',)
+        )
+
+        st.text_input('検索クエリ', key='query', on_change=update_state, args=('query',))
+        st.button('検索', on_click=search_hotel)
+    
+    with col2:
+        st.text_area('検索結果', '', height=600, key='result', on_change=update_state, args=('result',))
 
 if __name__ == '__main__':
     main()
