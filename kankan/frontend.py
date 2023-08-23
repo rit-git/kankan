9026c48cbbcb472e07d6b32f30958ca2c0762905
nishiharu <checompetition@163.com>
2023-08-23 09:45:25 +0900

    backend done

---
+++ kankan/frontend.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def main():
+    st.set_page_config(
+        page_title='Kankan',
+        layout='wide',
+    )
+    st.title('KanKan：観光スポット検索エンジン')
+    
+
+if __name__ == '__main__':
+    main()
