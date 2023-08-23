fc41fc3e6c781f2f8a300a3c31dfd33fd542be97
nishiharu <checompetition@163.com>
2023-08-23 13:51:18 +0900

    prototype done

--- kankan/backend.py
+++ kankan/backend.py
@@ -1,105 +1,30 @@
-import sys
-sys.path.append('.')
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
 
-import os
-import openai
-from openai.embeddings_utils import get_embedding, cosine_similarity
+class KankanRequest(BaseModel):
+    tdfk: str
+    query: str
 
-from vecscan import PyVectorScanner, PyVectorGroups
-from array import array
+class KankanAPI(FastAPI):
+    def __init__(self):
+        super().__init__()
 
-import collections
-import tqdm
-import heapq
+        self.post("/api/search")(self.search)
 
-n_best = 10
-thr=0.50
-model_name = 'ada' 
-todofuken_name = 'nagano'
-openai.api_key=os.environ["OPENAI_API_KEY"]
-vec_file_path = f'./dataset/sentvecs.{model_name}.kuchikomi_report.vec.{todofuken_name}'
-raw_data_path = f'./dataset/jalan_kanko.csv.{todofuken_name}.csv'
-query_file_path = './dataset/query_debug.txt'
-
-
-def search_reviews(query_text, scanner, **kwargs):
-    if model_name == 'ada':
-        embedding = array('f', get_embedding(query_text, engine='text-embedding-ada-002'))
-    ret = scanner.n_best_vecs(
-        query_vec=embedding,
-        n_best=100000,
-        openmp=True,
-        debug=True,
-        threshold=thr
+    def search(self, req: KankanRequest):
+        res = {
+            'result': req.tdfk + '|||' + req.query
+        }
+        return res
+    
+def main():
+    app = KankanAPI()
+    uvicorn.run(
+        app,
+        port=21345,
+        root_path='/app/kankan'
     )
-    return ret
-
-def print_kuchikomi_results(raw_data, scan_results):
-    scan_results_len = scan_results.size()
-
-    for i in range(min(n_best, scan_results_len)):
-        idx, score = scan_results.get(i)
-        print(raw_data[idx].strip() + ' ||| ' + str(score))
-    print('')
-
-
-
-def print_spot_results2(raw_data, scan_results):
-    spot_result = {}
-    full_odk_set = set()
-    scan_results_len = scan_results.size()
-    for i in range(scan_results_len):
-        idx, score = scan_results.get(i)
-        fields = raw_data[idx].strip().split(',')
-        kuchikomi_id, odk_id, other = fields[0], fields[1], ','.join(fields[2:])
-        if odk_id not in spot_result:
-            spot_result[odk_id] = {'score': 0.0, 'kuchikomi': [], 'cnt': 0}
-        if spot_result[odk_id]['cnt'] < n_best:
-            spot_result[odk_id]['kuchikomi'].append((score, other))
-            spot_result[odk_id]['score'] += score
-            if spot_result[odk_id]['cnt'] > 0:
-                spot_result[odk_id]['score'] /= 2
-            spot_result[odk_id]['cnt'] += 1
-        if spot_result[odk_id]['cnt'] == n_best:
-            full_odk_set.add(odk_id)
-            if len(full_odk_set) >= n_best:
-                break
-
-    if len(full_odk_set) == n_best:
-        result_list = [spot_result[k] for k in full_odk_set]
-    else:
-        result_list = spot_result.values()
-    result_list = heapq.nlargest(n_best, result_list, key=lambda x: x['score'])
-    for spot_rank, kuchikomi_list in enumerate(result_list):
-        print('Rank: {}, Score: {}'.format(spot_rank + 1, kuchikomi_list['score']))
-        for kuchikomi in kuchikomi_list['kuchikomi']:
-            print('{} ||| {}'.format(kuchikomi[0], kuchikomi[1]))
 
 if __name__ == '__main__':
-    query_list = []
-    for line in open(query_file_path):
-        line = line.strip()
-        if len(line) > 0:
-            query_list.append(line)
-    
-    scanner = PyVectorScanner(vec_file_path, '')
-    raw_data = []
-
-    for line_idx, line in enumerate(open(raw_data_path)):
-        if line_idx == 0:
-            continue
-        else:
-            raw_data.append(line)
-
-
-
-    print('start searching...', file=sys.stderr)
-    for query in query_list:
-        print('*'*40)
-        print(query)
-        if model_name == 'ada':
-            ret_lists = search_reviews(query, scanner)
-
-        scan_results = ret_lists[0]
-        print_spot_results2(raw_data, scan_results)
-
+    main()
--- kankan/frontend.py
+++ kankan/frontend.py
@@ -1,4 +1,20 @@
 import streamlit as st
+import requests
+
+def search_hotel():
+    response = requests.post(
+        'http://localhost:21345/api/search',
+        json={
+            'tdfk': st.session_state['tdfk'],
+            'query': st.session_state['query']
+        }
+    )
+    if response.status_code == 200:
+        response = response.json()
+        st.session_state['result'] = '***  {}   ***'.format(response['result'])
+
+def update_state(key):
+    st.session_state[key] = st.session_state[key]
 
 def main():
     st.set_page_config(
@@ -6,7 +22,11 @@ def main():
         layout='wide',
     )
     st.title('KanKan：観光スポット検索エンジン')
-    
+
+    st.selectbox('都道府県',('日本全国', '東京都', '大阪府', '長野県', '沖縄県'), key='tdfk', on_change=update_state, args=('tdfk',))
+    st.text_input('検索クエリ', 'デートに最適', key='query', on_change=update_state, args=('query',))
+    st.button('検索', on_click=search_hotel)
+    st.text_area('検索結果', '', key='result', on_change=update_state, args=('result',))
 
 if __name__ == '__main__':
     main()
